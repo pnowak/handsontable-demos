ebc7b8e8cf4cb3a2fdd30e562e80011d81d941f0
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-07-04 22:14:05 +0200

    HotChart class update

--- Highcharts/src/app.js
+++ Highcharts/src/app.js
@@ -1,24 +1,22 @@
-import Handsontable from 'handsontable';
+'use strict';
+
 import numbro from 'numbro';
 import moment from 'moment';
 import pikaday from 'pikaday';
 import Zeroclipboard from 'zeroclipboard';
+import Handsontable from 'handsontable';
 import Highcharts from 'highcharts';
 
 class HotChart {
-	constructor(hotOpt, charOpt) {
-		this.chart = new Highcharts.Chart(document.getElementById('container'), charOpt = this.charOptions());
-		this.hot = new Handsontable(document.getElementById('root'), hotOpt = this.hotOptions());
+	constructor(hotRoot, charRoot) {
+		this.hot = new Handsontable(document.getElementById(hotRoot), this.hotOptions());
+		this.chart = new Highcharts.Chart(document.getElementById(charRoot), this.charOptions());
 	}
 
 	charOptions() {
 		return {
 			title: {
 		        text: 'HighChart & Handsontable'
-		    },
-
-		    subtitle: {
-		        text: 'Plain'
 		    },
 
 		    xAxis: {
@@ -28,37 +26,31 @@ class HotChart {
 		    series: [{
 		        type: 'column',
 		        colorByPoint: true,
-		        data: this.hotOptions().data //data from Table
+		        data: this.hotOptions().data[0] //data from Table
 		    }]
-        }
+	    }
 	}
 
 	hotOptions() {
 		return {
 			data: [
-				["239.65","0.000128","-0.2379","47.044"],
-				["238.99","0.0106","-0.2435","5.11"],
-				["231.26","0.0066","-0.2521","7.571"],
-				["239.12","0.0082","-0.2454","16.429"],
-				["255.07","0.0091","-0.2017","252"],
-				["238.91","0.0077","-0.2437","995"],
-				["211.51","0.0089","-0.1880","4.28"],
-				["210.65","0.0078","-0.1930","2.521"],
-				["205.06","0.0107","-0.2251","96"],
-				["212.41","0.0085","-0.1949","456"],
-				["227.94","0.0158","-0.1363","49"],
-				["211.28","0.0078","-0.1765","19"],
-				["1486.97","0.0112","-0.2310","168"],
-				["1310.00","-0.01812","-0.3310","0"],
-				["1497.50","0.0051","-0.2309","160"]
+				[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 			],
-			colHeaders: ["Price", "1D Chg", "YTD Chg", "Vol BTC"],
+			colHeaders: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 			rowHeaders: true,
 			columns: [
-			    {type: 'numeric', format: '$0,0.00'},
-			    {type: 'numeric', format: '0.00%'},
-			    {type: 'numeric', format: '0.00%'},
-			    {type: 'numeric', format: '0.00'}
+			    {type: 'numeric'},
+			    {type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'},
+				{type: 'numeric'}
 			]
 		}
 	}
