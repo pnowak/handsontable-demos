ae698b4930b8f003c765ae42cbcb98636c099828
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-07-16 11:45:21 +0200

    highcharts listen handsontable

--- Highcharts/index.js
+++ Highcharts/index.js
@@ -8,13 +8,17 @@ hotChart.hot.addHook('beforeChange', function(changes, source) {
     if (source === 'loadData' || source === 'internal' || changes.length > 1) {
     	return;
     }
-    
+
     let col = changes[0][1];
     let value = changes[0][3];
 
     if (hotChart.chart.series) {
         hotChart.chart.series[0].data[col].update(value);
-    } else {
+    }
+
+    if (hotChart.chart.dataProvider) {
         hotChart.chart.dataProvider[col].value = value;
+
+        hotChart.chart.validateNow(true);
     }
 });
