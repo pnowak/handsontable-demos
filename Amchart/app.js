850f3146f0a6506335b8db3a618946ba13e2ab0b
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-07-22 18:18:16 +0200

    Amchart class

---
+++ Amchart/app.js
@@ -0,0 +1,77 @@
+'use strict';
+
+import 'amcharts3';
+import 'amcharts3/amcharts/serial';
+import 'amcharts3/amcharts/pie';
+import 'amcharts3/amcharts/themes/light';
+import { hotOptions } from '.././src/app';
+
+class AmChart {
+	constructor(amRoot, amOptions) {
+		this.amcharts = AmCharts.makeChart(amRoot, this.amOptions());
+	}
+
+	valueChanged(col, value) {
+		this.amcharts.dataProvider[col].value = value;
+
+		this.amcharts.validateNow(true);
+	}
+
+	amOptions() {
+		return {
+			'type': 'serial',
+			'theme': 'light',
+			'dataProvider': this.merge(hotOptions().colHeaders, hotOptions().data[0]),
+			'valueAxes': [ {
+					'gridColor': '#FFFFFF',
+					'gridAlpha': 0.2,
+					'dashLength': 0
+			} ],
+			'gridAboveGraphs': true,
+			'startDuration': 1,
+			'graphs': [ {
+					'balloonText': '[[category]]: <b>[[value]]</b>',
+					'fillAlphas': 0.8,
+					'lineAlpha': 0.2,
+					'type': 'column',
+					'valueField': 'value'
+			} ],
+			'chartCursor': {
+					'categoryBalloonEnabled': false,
+					'cursorAlpha': 0,
+					'zoomable': false
+			},
+			'categoryField': 'key',
+			'categoryAxis': {
+					'gridPosition': 'start',
+					'gridAlpha': 0,
+					'tickPosition': 'start',
+					'tickLength': 20
+			},
+			'export': {
+					'enabled': true
+			}
+		}
+	}
+
+	merge(key, value) {
+		let destination = [];
+
+		for (let i = 0, k = key.length; i < k; i += 1) {
+			for (let j = 0, v = value.length; j < v; j += 1) {
+				if (i === j) {
+					let obj = {};
+
+				  	obj.key = key[i];
+					obj.value = value[j];
+
+				 	destination.push(obj);
+				}
+			}
+		}
+
+		return destination;
+	}
+}
+
+export default AmChart;
