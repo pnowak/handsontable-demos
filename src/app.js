6e17cdc66cfbdc172854411036be423af79f70c8
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-07-30 23:14:41 +0200

    formatting

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 'use strict';
 
-import numbro from 'numbro';
-import moment from 'moment';
-import pikaday from 'pikaday';
-import Zeroclipboard from 'zeroclipboard';
 import Handsontable from 'handsontable';
 
 export default class HotChart {
@@ -16,23 +12,23 @@ export default class HotChart {
 export function hotOptions() {
 	return {
 		data: [
-				[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+			[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 		],
 		colHeaders: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 		rowHeaders: true,
 		columns: [
-		    {type: 'numeric'},
-		    {type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'},
-				{type: 'numeric'}
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'},
+			{type: 'numeric'}
 		]
 	}
 }
