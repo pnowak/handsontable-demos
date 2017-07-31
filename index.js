7cc13659e7b749c3d1f1387869f50ef09ab4a933
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-07-31 20:58:38 +0200

    switch charts

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ import HotChart from './src/app';
 import HighChart from './Highcharts/app';
 import AmChart from './amCharts/app';
 
-const highChart = new HighChart('container');
-const amChart = new AmChart('chartdiv');
+const highChart = new HighChart('highcharts');
+const amChart = new AmChart('amcharts');
 const hotChart = new HotChart('root', highChart, amChart);
+
+const buttons = document.getElementById('buttons');
+
+buttons.addEventListener('click', (e) => {
+    if (e.target.nodeName.toLowerCase() === "button") {
+        hotChart.charts.forEach(function (chart, index) {
+            let value = e.target.value;
+            let name = chart.name;
+
+            if (value === name) {
+                document.getElementById(value).classList.remove('disappear');
+            } else {
+                document.getElementById(name).classList.add('disappear');
+            }
+        });
+    }
+}, false);
 
 hotChart.hot.addHook('beforeChange', function(changes, source) {
     if (source === 'loadData' || source === 'internal' || changes.length > 1) {
