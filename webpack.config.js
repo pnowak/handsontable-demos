9fa2fcc813172978fc437f341d30ab420eb7869c
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-08-07 20:34:20 +0200

    babel jasmine

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = [
   {
     entry: `${__dirname}/src/index.js`,
@@ -8,21 +6,15 @@ module.exports = [
       filename: 'bundle.js',
     },
     module: {
-      noParse: [path.join(__dirname, 'node_modules/handsontable/dist/handsontable.full.js')],
       loaders: [
         {
           test: /\.js$/,
           loader: 'babel-loader',
           query: {
-            presets: ['es2015'],
+            presets: ['env'],
           },
         },
       ],
-    },
-    resolve: {
-      alias: {
-        handsontable: path.join(__dirname, 'node_modules/handsontable/dist/handsontable.full.js'),
-      },
     },
     stats: {
       colors: true,
