b9a1df7052958c62b69660c90b27161c75d6f590
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2017-09-07 15:07:44 +0200

    test drag corner

--- tests/test.js
+++ tests/test.js
@@ -3,14 +3,20 @@ import { Selector } from 'testcafe';
 fixture `Handsontable for charts`
   .page `file:///Users/piotrnowak/handsontable-demos/src/index.html`;
 
-test('Charts are hidden after the page loads', async t => {
+const hotTable = Selector('#root .ht_master .htCore tbody td');
+const corner = Selector('.wtBorder.current.corner');
+const firstTd = hotTable.nth(0);
+const secondTd = hotTable.nth(1);
+const lastTd = hotTable.nth(11);
+
+test('Charts are hidden after the page loads', async (t) => {
   const disappears = Selector('.items').child('.disappear').count;
 
   await t
     .expect(disappears).eql(2);
 });
 
-test('After clicked on the highcharts button the highcharts div appears', async t => {
+test('After clicked on the highcharts button the highcharts div appears', async (t) => {
   const highchartsButton = Selector('button[value=highcharts');
   const highchartsDiv = Selector('#highcharts');
 
@@ -19,7 +25,7 @@ test('After clicked on the highcharts button the highcharts div appears', async 
     .expect(highchartsDiv.classNames).notContains('disappear');
 });
 
-test('After clicked on the amCharts button the amCharts div appears', async t => {
+test('After clicked on the amCharts button the amCharts div appears', async (t) => {
   const amChartsButton = Selector('button[value=amCharts');
   const amChartsDiv = Selector('#amCharts');
 
@@ -28,25 +34,31 @@ test('After clicked on the amCharts button the amCharts div appears', async t =>
     .expect(amChartsDiv.classNames).notContains('disappear');
 });
 
-test('Handsontable drag first td value to second td', async t => {
-  const hotTable = Selector('#root .ht_master .htCore tbody td');
-  const firstTd = hotTable.nth(0);
-  const secondTd = hotTable.nth(1);
+test('Handsontable drag down not working', async (t) => {
+  const firstTr = Selector('#root .ht_master .htCore tbody tr');
 
   await t
-    .drag(firstTd, 39, 2)
-    .expect(secondTd.textContent).eql('30');
+    .click(firstTd)
+    .click(corner)
+    .drag(corner, 0, 40, { speed: 0.5 })
+    .expect(firstTr.nextSibling('tr').count)
+      .eql(0);
 });
 
-test('Handsontable copy first td value and paste in the second td', async t => {
-  const hotTable = Selector('#root .ht_master .htCore tbody td');
-  const firstTd = hotTable.nth(0);
-  const secondTd = hotTable.nth(1);
+test('Handsontable drag first td value to last td', async (t) => {
+  await t
+    .click(firstTd)
+    .click(corner)
+    .drag(corner, 500, -3, { speed: 0.5 })
+    .expect(lastTd.textContent)
+      .eql('30');
+});
 
+test('Handsontable copy first td value and paste in the second td', async (t) => {
   await t
     .expect(firstTd.textContent).eql('30')
     .expect(secondTd.textContent).eql('72')
-    .click(firstTd, { ctrl: true } )
-    .typeText(secondTd, '', { paste: true })
-    .expect(secondTd.textContent).eql('30');
+    .typeText(secondTd, '30', { replace: true, paste: true, speed: 0.5 })
+    .expect(secondTd.textContent)
+      .eql('30');
 });
