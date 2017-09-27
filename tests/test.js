0ee6c4692c44f31dd23ce3265f2ff01f1d906e37
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2017-09-27 09:14:50 +0200

    test highcharts

--- tests/test.js
+++ tests/test.js
@@ -1,64 +1,61 @@
 import { Selector } from 'testcafe';
 
 fixture `Handsontable for charts`
-  .page `file:///Users/piotrnowak/handsontable-demos/src/index.html`;
+  .page `../www/index.html`;
 
 const hotTable = Selector('#root .ht_master .htCore tbody td');
+const highChart = Selector('#chart svg .highcharts-series-group .highcharts-series rect');
 const corner = Selector('.wtBorder.current.corner');
 const firstTd = hotTable.nth(0);
-const secondTd = hotTable.nth(1);
+const sixTd = hotTable.nth(5);
 const lastTd = hotTable.nth(11);
 
-test('Charts are hidden after the page loads', async (t) => {
-  const disappears = Selector('.items').child('.disappear').count;
+test('Highcharts are visible after the page loads', async (t) => {
+  const oneChart = Selector('#chart').child('.highcharts-container').count;
 
   await t
-    .expect(disappears).eql(2);
+    .expect(oneChart).eql(1);
 });
 
-test('After clicked on the highcharts button the highcharts div appears', async (t) => {
-  const highchartsButton = Selector('button[value=highcharts');
-  const highchartsDiv = Selector('#highcharts');
-
-  await t
-    .click(highchartsButton)
-    .expect(highchartsDiv.classNames).notContains('disappear');
-});
-
-test('After clicked on the amCharts button the amCharts div appears', async (t) => {
-  const amChartsButton = Selector('button[value=amCharts');
-  const amChartsDiv = Selector('#amCharts');
-
-  await t
-    .click(amChartsButton)
-    .expect(amChartsDiv.classNames).notContains('disappear');
-});
-
-test('Handsontable drag down not working', async (t) => {
-  const firstTr = Selector('#root .ht_master .htCore tbody tr');
+test('When Handsontable drag down in Highcharts nothing happens', async (t) => {
+  const firstHighchart = highChart.nth(0);
+  const initialHeight = firstHighchart.getAttribute('height');
 
   await t
     .click(firstTd)
     .click(corner)
     .drag(corner, 0, 40, { speed: 0.5 })
-    .expect(firstTr.nextSibling('tr').count)
-      .eql(0);
+    .expect(firstHighchart.classNames)
+      .contains('highcharts-color-0')
+    .expect(firstHighchart.getAttribute('height'))
+      .eql(await initialHeight);
 });
 
-test('Handsontable drag first td value to last td', async (t) => {
+test('When Handsontable drag first td value to last td Highcharts change value', async (t) => {
+  const lastHighchart = highChart.nth(11);
+  const initialHeight = lastHighchart.getAttribute('height');
+
   await t
     .click(firstTd)
     .click(corner)
-    .drag(corner, 500, -3, { speed: 0.5 })
+    .drag(corner, 550, -3, { speed: 0.5 })
     .expect(lastTd.textContent)
-      .eql('30');
+      .eql('30')
+    .expect(lastHighchart.getAttribute('height'))
+      .notEql(await initialHeight);
 });
 
-test('Handsontable copy first td value and paste in the second td', async (t) => {
+test('When Handsontable change value Highcharts change value too', async (t) => {
+  const sixHighchart = highChart.nth(5);
+
   await t
-    .expect(firstTd.textContent).eql('30')
-    .expect(secondTd.textContent).eql('72')
-    .typeText(secondTd, '30', { replace: true, paste: true, speed: 0.5 })
-    .expect(secondTd.textContent)
-      .eql('30');
+    .expect(sixTd.textContent)
+      .eql('176')
+    .typeText(sixTd, '1', { replace: true })
+    .pressKey('1')
+    .click(Selector('#chart'))
+    .expect(sixTd.textContent)
+      .eql('1')
+    .expect(sixHighchart.getAttribute('height'))
+      .eql('1');
 });
